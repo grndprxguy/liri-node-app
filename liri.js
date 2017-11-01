fcc1d4dd488bac70b50b9fa8ddefaad28ff2d52c
Matthew Nagy <grndprxguy@gmail.com>
2017-11-01 10:54:52 -0400

    all api's working

--- liri.js
+++ liri.js
@@ -4,7 +4,6 @@ var inquirer = require("inquirer");
 var Twitter = require('twitter');
 var Spotify = require('node-spotify-api');
 var fs = require("fs");
-var movieName = "";
 
 var client = new Twitter({
   consumer_key: keys.twitterKeys.consumer_key,
@@ -24,59 +23,60 @@ var input1 = JSON.stringify(nodeArgs.slice(3));
 var input = encodeURIComponent(input1);
 str = "";
 
-// var firstPrompt = [{
-// 	type: "list",
-// 	name: "doWhat",
-// 	message: "Please enter a command",
-// 	choices: ["my-tweets","spotify-this-song","movie-this","do-what-it-says"]
-// }];
-
-// var songPrompt = [{
-// 	type: "input",
-// 	name: "songName",
-// 	message: "What song would you like to look up?"
-// }]
-
-// // function liriApp(answers){
-// // 	console.log("end")
-// // 	};
-
 // function to look up tweets
 function tweets() {
-	client.get('statuses/user_timeline',function(err, response) {
+	client.get('statuses/user_timeline',function(err, data) {
 		if (err) {
     	return console.log('Error occurred: ' + err);
   	};
   	for (i=0;i < 5; i++) {
- 	console.log(JSON.stringify(response[i].text));
- }
- 	})
+  		console.log("----------------------")
+ 		console.log(JSON.stringify(data[i].text));
+ 	}
+ })
 };
 
 // function to look up spotify
 function spotifyResp() {
-	console.log("spotify");
-	spotify.search({type: 'track', query: "'" + input + "'"}, function(err,data) {
-		 if (err) {
-    return console.log('Error occurred: ' + err);
-  }
-  if (input !== undefined){
+	if (input !== "%5B%5D"){
+		console.log(input)
+		spotify.search({type: 'track', query: "'" + input + "'"}, function(err,data) {
+			if (err) {
+    	return console.log('Error occurred: ' + err);
+  	}
   	var track = data.tracks.items[0];
+  	console.log("----------------------")
   	console.log("Artists: " + track.artists[0].name)
   	console.log("Album: " + track.album.name)
   	console.log("Track Name: " + track.name);
   	console.log("Preview: " + track.href);
-  }
-  
-	})
+  	console.log("----------------------")
+  });
+  } else {
+  	input = "The Sign";
+  	console.log(input)
+	spotify.search({type: 'track', query: "the+sign"}, function(err,data) {
+		if (err) {
+	return console.log('Error occurred: ' + err);
+	}
+  	var track = data.tracks.items[0];
+  	console.log("----------------------")
+  	console.log("Artists: " + track.artists[0].name)
+  	console.log("Album: " + track.album.name)
+  	console.log("Track Name: " + track.name);
+  	console.log("Preview: " + track.href);
+  	console.log("----------------------")
+  });
 };
+}
 
 // function to look up OMDB
 function omdbLookup() {
 	console.log("omdb");
 	var url = "http://www.omdbapi.com/?t=" + input + "&y=&plot=short&apikey=40e9cece";
-	request(url, function(err,response,body) {
+	request(url, function(err,data,body) {
 		var obj = JSON.parse(body);
+		console.log("----------------------")
 		console.log("Title: " + obj.Title);
 		console.log("Year: " + obj.Year);
 		console.log("IMDB Rating: " + obj.IMDB);
@@ -85,41 +85,39 @@ function omdbLookup() {
 		console.log("Language: " + obj.Language);
 		console.log("Plot: " + obj.Plot);
 		console.log("Actors: " + obj.Actors);
+		console.log("----------------------")
 	});
 }
 
 function whatItSays() {
 	fs.readFile("random.txt", "utf8", function(error, data) {
-		console.log(data);
+		if (error) {
+			console.log("Something went wrong " + error)
+		} else {
+			result = data.split(",");
+			action = result[0];
+			input = result[1];
+			argTest(action, input);
+		}
 	});
 };
 
+function argTest() {
+	for (var i = 2; i < nodeArgs.length; i++) {
+		str = str + " " + nodeArgs[i];
+	}
 
-// inquirer.prompt(firstPrompt).then(function(answer) {
-// 	console.log(answer);
-// 	if (answer.doWhat = "my-tweets") {
-// 		tweets();
-// 	} else if 
-// 		(answer.doWhat = "spotify-this-song"){
-// 			inquirer.prompt(songPrompt).then(function(songAnswer) {
-// 				spotifyResp();
-// 			})
-// 		}
-// });
-
-for (var i = 2; i < nodeArgs.length; i++) {
-	str = str + " " + nodeArgs[i];
+	if (action == "my-tweets") {
+		tweets();
+	} else if
+	(action == "spotify-this-song") {
+		spotifyResp();
+	} else if 
+	(action == "movie-this") {
+		omdbLookup();
+	} else if 
+	(action == "do-what-it-says") {
+		whatItSays();
+	}
 }
-
-if (action == "my-tweets") {
-	tweets();
-} else if
-(action == "spotify-this-song") {
-	spotifyResp();
-} else if 
-(action == "movie-this") {
-	omdbLookup();
-} else if 
-(action == "do-what-it-says") {
-	whatItSays();
-}
+argTest();
